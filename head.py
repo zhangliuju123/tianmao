bf2a0a889dd714aa868acd495e4662cf487e5290
zhangliujun123 <962502635@qq.com>
2021-01-14 23:10:24 +0800

    更改了浏览器设置

--- head.py
+++ head.py
@@ -17,6 +17,8 @@ def create_bs_driver(type="firefox", headless=False):
         driver = webdriver.Firefox(firefox_options=firefox_opt)
     elif type == "chrome":  # 谷歌浏览器
         chrome_opt = webdriver.ChromeOptions()
+        with open('G:\PythonWork\MyScrapy\stealth.min.js') as f:
+            js = f.read()
         chrome_opt.add_argument(f'--window-position={217},{172}')
         chrome_opt.add_argument(f'--window-size={1200},{1000}')
         chrome_opt.add_experimental_option("excludeSwitches", ['enable-automation'])
@@ -24,10 +26,13 @@ def create_bs_driver(type="firefox", headless=False):
         # chrome_opt.add_experimental_option("prefs",
         #                                    {"profile.managed_default_content_settings.images": 2})  # 加快请求速度,设置禁止加载图片
         driver = webdriver.Chrome(chrome_options=chrome_opt)
-        # 屏蔽selenium检测,提前运行js的方法，把window.navigator.webdriver设为"undefined"
         driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-            "source": """Object.defineProperty(navigator, 'webdriver', {get: () => undefined})""",
+            "source": js
         })
+        # 屏蔽selenium检测,提前运行js的方法，把window.navigator.webdriver设为"undefined"
+        # driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        #     "source": """Object.defineProperty(navigator, 'webdriver', {get: () => undefined})""",
+        # })
     else:
         return None
     return driver
